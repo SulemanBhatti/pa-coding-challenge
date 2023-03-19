0be9663cf598c1d824a244fb14b3db4634a2e20d
SulemanBhatti <sulemanbhatti.business@gmail.com>
2023-03-19 11:50:54 +0100

    adding styles as per design configurations

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -6,11 +6,11 @@ export const AuthForm: React.FC = () => {
     <div className="container">
       <div className="form__wrapper">
         <form>
-          <div className="form__input__wrapper">
+          <div className="first__column__wrapper">
             <label htmlFor="Email">Email</label>
             <input type="Email" placeholder="Email" />
           </div>
-          <div className="form__input__wrapper">
+          <div className="second__column__wrapper">
             <label htmlFor="Password">Password</label>
             <input type="Password" placeholder="Password" />
             <button type="submit">Submit</button>
