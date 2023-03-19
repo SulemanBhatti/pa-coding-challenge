598bfdca460afe2c06066459f9eaf2e0eb0edb90
SulemanBhatti <sulemanbhatti.business@gmail.com>
2023-03-19 11:28:16 +0100

    css grid and column alignment fixations

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -3,18 +3,20 @@ import './AuthForm.css';
 
 export const AuthForm: React.FC = () => {
   return (
-    <div className="form__wrapper">
-      <form>
-        <div className="form__input">
-          <label htmlFor="Email">Email</label>
-          <input type="Email" placeholder="Email" />
-        </div>
-        <div className="form__input">
-          <label htmlFor="Password">Password</label>
-          <input type="Password" placeholder="Password" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+    <div className="container">
+      <div className="form__wrapper">
+        <form>
+          <div className="form__input__wrapper">
+            <label htmlFor="Email">Email</label>
+            <input type="Email" placeholder="Email" />
+          </div>
+          <div className="form__input__wrapper">
+            <label htmlFor="Password">Password</label>
+            <input type="Password" placeholder="Password" />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
